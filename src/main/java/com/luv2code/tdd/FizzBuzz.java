cd75fea0f93d6e66f7b4f23bdd8d189efd395823
Josef Kudláček <j.k.kudlacek@gmail.com>
2024-07-25 14:16:52 +0200

    TDD - FizzBuzz refactor

--- src/main/java/com/luv2code/tdd/FizzBuzz.java
+++ src/main/java/com/luv2code/tdd/FizzBuzz.java
@@ -8,6 +8,24 @@ public class FizzBuzz {
     // If number is NOT divisible by 3 or 5, then print the number
 
     public static String compute(int i) {
+        StringBuilder result = new StringBuilder();
+        if (i % 3 == 0) {
+            result.append("Fizz");
+        }
+
+        if (i % 5 == 0) {
+            result.append("Buzz");
+        }
+
+        if (result.isEmpty()) {
+            result.append(i);
+        }
+
+        return result.toString();
+    }
+
+    /*
+    public static String compute(int i) {
         if ((i % 3 == 0) && (i % 5 == 0)) {
             return "FizzBuzz";
         }
@@ -20,4 +38,5 @@ public class FizzBuzz {
 
         return String.valueOf(i);
     }
+    */
 }
