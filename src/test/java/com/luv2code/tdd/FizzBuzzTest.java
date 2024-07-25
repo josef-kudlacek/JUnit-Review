11b953e171f7b57ccd8143cbf37ca30bd8689b33
Josef Kudláček <j.k.kudlacek@gmail.com>
2024-07-25 14:06:12 +0200

    TDD - division by 3 or 5

--- src/test/java/com/luv2code/tdd/FizzBuzzTest.java
+++ src/test/java/com/luv2code/tdd/FizzBuzzTest.java
@@ -2,13 +2,12 @@ package com.luv2code.tdd;
 
 import org.junit.jupiter.api.*;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class FizzBuzzTest {
 
     // If number is divisible by 3, print Fizz
-    // If number is divisible by 5, print Buzz
     // If number is divisible by 3 and 5, print FizzBuzz
     // If number is NOT divisible by 3 or 5, then print the number
 
@@ -16,6 +15,19 @@ class FizzBuzzTest {
     @Test
     @Order(1)
     void testForDivisibleByThree() {
-        fail("fail");
+        String expected = "Fizz";
+
+        assertEquals(expected, FizzBuzz.compute(3), "Should return Fizz");
+    }
+
+    // If number is divisible by 5, print Buzz
+
+    @DisplayName("Divisible by Five")
+    @Test
+    @Order(1)
+    void testForDivisibleByFive() {
+        String expected = "Buzz";
+
+        assertEquals(expected, FizzBuzz.compute(5), "Should return Buzz");
     }
 }
